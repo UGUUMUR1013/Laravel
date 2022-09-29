f422aa54805c882992628b914732e5a88bf4f7ef
Jargalaa <103235737+Jargalaa@users.noreply.github.com>
2022-09-29 14:14:07 +0800

    lab 5

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+
+@section('content')
+      <h1>Welcome to Laravel</h1>
+      <p> This is the Laravel application from the "Laravel From Scrach" Youtube series</p>
+@endsection
