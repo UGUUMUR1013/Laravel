c5eef97bc401296c9145550d7590a34c7513558d
Jargalaa <103235737+Jargalaa@users.noreply.github.com>
2022-09-29 14:14:41 +0800

    lab 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\HTTP\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
 });
+
+
+// Route::get('/hello', function () {
+//     //return view('welcome');
+//     return '<h1>Hello World</h1>';
+// });
+
+
+
+// Route::get('/about', function(){
+//     return view('pages.about');
+// });
+
+ Route::get('/', 'App\HTTP\Controllers\PagesController@index');
+ Route::get('/about', 'App\HTTP\Controllers\PagesController@about'); 
+ Route::get('/services', 'App\HTTP\Controllers\PagesController@services'); 
+   //asuuh 
+
+// Route::get('/about', function () {     //asuuh
+//     return view('pages.about');
+// });
+
+
+
+// Route::get('/users/{id}/{name}', function ( $id, $name) {
+//     return 'This is user '.$name. ' with an id of '. $id;
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
